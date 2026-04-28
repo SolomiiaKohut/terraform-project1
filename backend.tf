4d3f33913fcd53bd360b145af94fbca5a0a9e9bd
SolomiiaKohut <solomiia.kohut.kn.2021@lpnu.ua>
2026-04-28 20:21:27 +0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,11 @@
 terraform {
   backend "s3" {
-    endpoint = "https://fra1.digitaloceanspaces.com"
     bucket   = "kohut-bucket"
     key      = "terraform.tfstate"
     region   = "us-east-1"
+    endpoints = {
+      s3 = "https://fra1.digitaloceanspaces.com"
+    }
 
 
     skip_credentials_validation = true
