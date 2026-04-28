24eb2cc988d76aad33f33a0a7cb5f7506335cdb6
SolomiiaKohut <solomiia.kohut.kn.2021@lpnu.ua>
2026-04-28 21:47:56 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,13 +10,9 @@ variable "ssh_fingerprint" {
 }
 
 variable "spaces_access_key" {
-  description = "Spaces access key"
-  type        = string
-  sensitive   = true
+
 }
 
 variable "spaces_secret_key" {
-  description = "Spaces secret key"
-  type        = string
-  sensitive   = true
+
 }
