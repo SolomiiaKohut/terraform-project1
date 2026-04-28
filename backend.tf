7c2409d90e006f25c2fe21409f2da7911a4c6829
SolomiiaKohut <solomiia.kohut.kn.2021@lpnu.ua>
2026-04-28 20:40:02 +0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -8,9 +8,6 @@ terraform {
       s3 = "https://fra1.digitaloceanspaces.com"
     }
 
-    skip_credentials_validation = true
-    # skip_metadata_api_check     = true
-    skip_requesting_account_id  = true
-    skip_region_validation      = true
+
   }
 }
