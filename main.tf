4b120df5a6f41f1fe355915aa6f46e4ca8341fe1
SolomiiaKohut <solomiia.kohut.kn.2021@lpnu.ua>
2026-04-28 19:14:21 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,8 +5,20 @@ terraform {
       version = "~> 2.0"
     }
   }
-}
 
+  backend "s3" {
+    bucket = "kohut-bucket"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+
+    endpoints = {
+      s3 = "https://fra1.digitaloceanspaces.com"
+    }
+
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+  }
+}
 
 provider "digitalocean" {
   token = var.do_token
