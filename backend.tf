0b822deb466eb8366768596f88b570208a2f5e4e
SolomiiaKohut <solomiia.kohut.kn.2021@lpnu.ua>
2026-04-28 20:30:29 +0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "kohut-bucket"
-    key    = "terraform.tfstate"
+    key    = "kohut-bucket/terraform.tfstate"
     region = "us-east-1"
 
     endpoints = {
