7f1de9d624b5a38f425f811c6a7a5c0fabfdc8ae
SolomiiaKohut <solomiia.kohut.kn.2021@lpnu.ua>
2026-04-28 18:09:46 +0300

    Create backend.tf

---
+++ terraform/backend.tf
@@ -0,0 +1,14 @@
+terraform {
+  backend "s3" {
+    bucket = "kohut-bucket"
+    key    = "terraform.tfstate"
+    region = "fra1"
+
+    endpoints = {
+      s3 = "https://fra1.digitaloceanspaces.com"
+    }
+
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+  }
+}
